851f39acd19101e6e786986552950a891197cc59
alibatasys <alibatasys@DESKTOP-56HNGC9.local>
2019-11-29 23:24:49 +0800

    First commit

---
+++ section11/src/main/java/org/packt/springboot22/vid01/Section11Application.java
@@ -0,0 +1,13 @@
+package org.packt.springboot22.vid01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section11Application.class, args);
+	}
+
+}
