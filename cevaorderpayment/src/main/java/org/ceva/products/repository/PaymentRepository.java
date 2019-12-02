2dab2d7d0d295cdd06ec2b845d087aee980470d5
alibatasys <alibatasys@DESKTOP-56HNGC9>
2019-12-02 16:54:46 +0800

    Untrack

--- cevaorderpayment/src/main/java/org/ceva/products/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ceva.products.repository;
-
-import org.ceva.products.model.PaymentProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<PaymentProduct, Long>{
-
-	
-}
--- cevaorderpayment/src/main/java/org/ceva/products/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package org.ceva.products.repository;
-
-import org.ceva.products.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-
-}
